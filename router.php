755cde7259ed01d9436529b9077246defdfc0dfe
Juan <juanbautistacuchiarelli@gmail.com>
2026-05-19 16:28:43 -0300

    agrego modificacion de archivos

--- router.php
+++ router.php
@@ -1,11 +1,7 @@
 <?php
 require_once __DIR__ . '/app/controllers/ConcertController.php';
 require_once __DIR__ . '/app/controllers/AuthController.php';
-<<<<<<< HEAD
 require_once __DIR__ . '/app/controllers/bandas.controller.php';
-=======
-
->>>>>>> 4e7772db04f094b9a25864ce9fc0483579a7664e
 require_once __DIR__ . '/app/middlewares/session.middleware.php';
 require_once __DIR__ . '/app/middlewares/guard.middleware.php';
 
